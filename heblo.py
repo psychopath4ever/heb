93003584d8db1c3774ca1f4fa1f4c99491a50241
t.beckert <elandir@gmx.net>
2020-03-31 09:54:04 +0200

    Just_initialation

---
+++ heblo.py
@@ -0,0 +1,17 @@
+#Version 0.0
+PlayGame()
+
+
+def PlayerTurn()
+
+def PlayGame()
+#player0 has 1s (WHITE) and player1 has 3s (BLACK)
+  board = [[1,1,0,0,0,0,3,3],
+           [1,1,0,0,0,0,3,3],
+           [0,0,0,0,0,0,0,0],
+           [1,1,0,0,0,0,3,3],
+           [1,1,0,0,0,0,3,3],
+           [0,0,0,0,0,0,0,0],
+           [1,1,0,0,0,0,3,3],
+           [1,1,0,0,0,0,3,3]]
+  PlayerTurn()
